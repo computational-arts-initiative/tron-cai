f97e12ed06acc4ef9843b2b1880e68b8d73f1d80
Ulric Wilfred <shaman.sir@gmail.com>
2021-11-17 17:22:01 +0100

    fill in with actual Products' decoders and GUI

--- src/JetBrains/Gui/Products.elm
+++ src/JetBrains/Gui/Products.elm
@@ -1,11 +1,36 @@
 module JetBrains.Gui.Products exposing (products)
 
 
+import Dict
+
 import Tron exposing (Tron)
 import Tron.Build as Tron
+import Tron.Style.PanelShape exposing (..)
 
-import JetBrains.Product as Product exposing (Product)
+import JetBrains.Product as Products exposing (Products)
 
 
-products : Tron (Maybe Product)
-products = Tron.root []
+products : Products -> Tron Products.Product
+products ps =
+    Tron.choiceBy
+        (ps
+            |> Dict.toList
+            |> List.filter (Tuple.first >> Products.hasIcon)
+            |> Tron.buttons
+            |> List.map (Tron.with (Tron.face << productIcon))
+            |> Tron.toSet Tuple.first
+        )
+        Products.default
+        (\(nameA, _) (nameB, _) -> nameA == nameB)
+        Tuple.second
+    -- |> Tron.shape (rows 4)
+    -- |> Tron.expand
+
+
+productIcon : ( String, Products.Product ) -> Tron.Face
+productIcon ( product, _ ) =
+    Tron.iconAt
+        [ "assets"
+        , "product-logos"
+        , (product |> Products.iconName |> Maybe.withDefault "none") ++ ".svg"
+        ]
--- src/JetBrains/Product.elm
+++ src/JetBrains/Product.elm
@@ -1,4 +1,107 @@
-module JetBrains.Product exposing (Product)
+module JetBrains.Product exposing
+    ( Product
+    , Products
+    , default
+    , decodeOne
+    , decode
+    , hasIcon
+    , iconName
+    )
 
 
-type alias Product = String
+import Dict exposing (Dict)
+import Json.Decode as D
+
+
+type alias Color = { r : Float, g : Float, b : Float}
+type alias Palette = List Color
+type alias Product = { code : String, palette : Palette }
+
+
+type alias Products = Dict String Product
+
+
+type alias Palettes =
+    List
+        { product : String
+        , palette: Palette
+        }
+
+
+default = ( "", { code = "", palette = [] } )
+
+
+decodeColor : D.Decoder Color
+decodeColor
+    = D.map3
+            Color
+            (D.field "r" D.int |> D.map (\r -> toFloat r / 255))
+            (D.field "g" D.int |> D.map (\g -> toFloat g / 255))
+            (D.field "b" D.int |> D.map (\b -> toFloat b / 255))
+
+decodeOne : D.Decoder Product
+decodeOne
+    = D.map2
+        (\code palette -> { code = code, palette = palette })
+        (D.field "code" D.string)
+        (D.field "palette" <| D.list decodeColor)
+
+
+decode : D.Decoder Products
+decode =
+    D.field "products"
+        <| D.dict decodeOne
+
+
+hasIcon : String -> Bool
+hasIcon product =
+    case iconName product of
+        Just _ -> True
+        Nothing -> False
+
+
+iconName : String -> Maybe String
+iconName product =
+    case product of
+        "JetBrains" -> Just "logojb"
+        "Space" -> Just "Space"
+        "IntelliJ IDEA" -> Just "IntelliJ-IDEA"
+        "PhpStorm" -> Just "PhpStorm"
+        "PyCharm" -> Just "PyCharm"
+        "RubyMine" -> Just "RubyMine"
+        "WebStorm" -> Just "WebStorm"
+        "CLion" -> Just "CLion"
+        "DataGrip" -> Just "DataGrip"
+        "DataSpell" -> Just "DataSpell"
+        "AppCode" -> Just "AppCode"
+        "GoLand" -> Just "GoLand"
+        "ReSharper" -> Just "ReSharper"
+        "ReSharper C++" -> Just "ReSharperCPP"
+        "dotCover" -> Just "dotCover"
+        "dotMemory" -> Just "dotMemory"
+        "dotPeek" -> Just "dotPeek"
+        "dotTrace" -> Just "dotTrace"
+        "Rider" -> Just "Rider"
+        "TeamCity" -> Just "TeamCity"
+        "YouTrack" -> Just "YouTrack"
+        "Upsource" -> Just "Upsource"
+        "Hub" -> Just "Hub"
+        "Kotlin" -> Just "Kotlin"
+        "Mono" -> Just "Mono"
+        "MPS" -> Just "MPS"
+        "IntelliJ IDEA Edu" -> Just "IntelliJ-IDEA-Edu"
+        "PyCharm Edu" -> Just "PyCharm-Edu"
+        "Datalore" -> Just "Datalore"
+        _ -> Nothing
+
+
+collectPalettes : Products -> Palettes
+collectPalettes =
+    Dict.toList
+        >> List.map
+            (\(productName, productData) ->
+                { product = productName
+                , palette = productData.palette
+                }
+            )
+        >> List.filter (.product >> hasIcon)
